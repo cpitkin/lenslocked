0ab596d3f51e41ebecfe8812568f0807c68d6e5f
Charlie Pitkin <charlie.pitkin@gmail.com>
2019-08-14 11:21:23 +0300

    Added dynamic layout in bootstrap template
    templates now use a single layout with dynamic content
    
    Signed-off-by: Charlie Pitkin <charlie.pitkin@gmail.com>

--- main.go
+++ main.go
@@ -13,14 +13,14 @@ var contactView *views.View
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := homeView.Template.Execute(w, nil); err != nil {
+	if err := homeView.Template.ExecuteTemplate(w, homeView.Layout, nil); err != nil {
 		panic(err)
 	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := contactView.Template.Execute(w, nil); err != nil {
+	if err := contactView.Template.ExecuteTemplate(w, contactView.Layout, nil); err != nil {
 		panic(err)
 	}
 }
@@ -37,9 +37,9 @@ func notFound(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
-	homeView = views.NewView("views/home.gohtml")
+	homeView = views.NewView("bootstrap", "views/home.gohtml")
 
-	contactView = views.NewView("views/contact.gohtml")
+	contactView = views.NewView("bootstrap", "views/contact.gohtml")
 
 	var nf http.Handler = http.HandlerFunc(notFound)
 
