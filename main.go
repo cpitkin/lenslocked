72bbdad191ae914ff10ea1b5d398c3732066f2e4
Charlie Pitkin <charlie.pitkin@gmail.com>
2019-08-15 12:40:21 +0300

    Added a signup form, Updated both layouts
    sign up form template wrapped in a panel
    bootstrap and material work on mobile
    
    Signed-off-by: Charlie Pitkin <charlie.pitkin@gmail.com>

--- main.go
+++ main.go
@@ -7,10 +7,13 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var homeView *views.View
-var contactView *views.View
-var faqView *views.View
-var notFoundView *views.View
+var (
+	homeView     *views.View
+	contactView  *views.View
+	faqView      *views.View
+	notFoundView *views.View
+	signupView   *views.View
+)
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -27,6 +30,11 @@ func faq(w http.ResponseWriter, r *http.Request) {
 	must(faqView.Render(w, nil))
 }
 
+func signup(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(signupView.Render(w, nil))
+}
+
 func notFound(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 	notFoundView.Render(w, nil)
@@ -41,11 +49,9 @@ func must(err error) {
 func main() {
 
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
-
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
-
 	faqView = views.NewView("material", "views/faq.gohtml")
-
+	signupView = views.NewView("bootstrap", "views/signup.gohtml")
 	notFoundView = views.NewView("material", "views/404.gohtml")
 
 	var nf http.Handler = http.HandlerFunc(notFound)
@@ -54,6 +60,8 @@ func main() {
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/faq", faq)
+	r.HandleFunc("/signup", signup)
+
 	r.NotFoundHandler = nf
 	http.ListenAndServe(":3000", r)
 }
